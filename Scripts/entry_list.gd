18a790859297b1b14e3a7b6162e7c06cf71a4f58
LucKy232 <11179336+LucKy232@users.noreply.github.com>
2026-01-26 19:49:37 +0100

    scroll to bottom by default or when adding a task

--- Scripts/entry_list.gd
+++ Scripts/entry_list.gd
@@ -3,50 +3,73 @@ class_name EntryList extends Control
 @export_file("*.tscn") var entry_scene
 @export_file("*.tscn") var day_summary_scene
 @onready var project_name: LineEdit = %ProjectName
+@onready var scroll_container: ScrollContainer = %ScrollContainer
 @onready var entries_container: VBoxContainer = %EntriesContainer
 var entries: Dictionary[int, Entry]
-var summaries: Dictionary[String, DaySummary]
+var summaries_day: Dictionary[String, DaySummary]
 var latest_entry_id: int
+var view_mode: ViewMode
 
-var show_summaries: bool = false
 signal project_name_changed
+
+enum ViewMode {
+	ALL,
+	TODAY,
+	THIS_WEEK,
+	THIS_MONTH,
+	DAY_SUMMARY,
+	WEEK_SUMMARY,
+	MONTH_SUMMARY,
+}
+
+
+func _ready() -> void:
+	visible = true
+	scroll_to_bottom()
+	visible = false
 
 
 # TODO buttons instead
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_summaries"):
-		show_summaries = !show_summaries
+		var show_summaries_day: bool = false
+		if view_mode == ViewMode.ALL:
+			view_mode = ViewMode.DAY_SUMMARY
+			show_summaries_day = true
+		elif view_mode == ViewMode.DAY_SUMMARY:
+			view_mode = ViewMode.ALL
+		
 		for eid in entries:
-			entries[eid].visible = !show_summaries
-		for s in summaries:
-			summaries[s].visible = show_summaries
+			entries[eid].visible = !show_summaries_day
+		for s in summaries_day:
+			summaries_day[s].visible = show_summaries_day
 	if Input.is_action_just_pressed("build_summaries"):
-		clear_summaries()
-		build_summaries()
+		clear_summaries_day()
+		build_summaries_day()
 
 
-func clear_summaries() -> void:
-	for d in summaries:
-		summaries[d].queue_free()
-	summaries.clear()
+func clear_summaries_day() -> void:
+	for d in summaries_day:
+		summaries_day[d].queue_free()
+	summaries_day.clear()
 
 
 # TODO only clear and rebuild today
-func build_summaries() -> void:
+func build_summaries_day() -> void:
 	#var today: String = Time.get_date_string_from_system()
 	for eid in entries:
 		var date: String = entries[eid].start_datetime.split(" ")[0]
-		if !summaries.has(date):
+		if !summaries_day.has(date):
 			var new: DaySummary = load(day_summary_scene).instantiate() as DaySummary
 			entries_container.add_child(new)
 			new.set_date(date)
 			new.add_active_duration(entries[eid].duration)
 			new.add_break_duration(entries[eid].break_duration)
-			new.visible = show_summaries
-			summaries[date] = new
+			new.visible = bool(view_mode == ViewMode.DAY_SUMMARY)
+			summaries_day[date] = new
 		else:
-			summaries[date].add_active_duration(entries[eid].duration)
-			summaries[date].add_break_duration(entries[eid].break_duration)
+			summaries_day[date].add_active_duration(entries[eid].duration)
+			summaries_day[date].add_break_duration(entries[eid].break_duration)
 
 
 func new_entry() -> int:
@@ -65,6 +88,12 @@ func populate_entries_from_dict(dict: Dictionary) -> void:
 	for e in dict["Entries"]:
 		var eid: int = new_entry()
 		entries[eid].set_data_from_json(dict["Entries"][e])
+		entries[eid].visible = bool(view_mode == ViewMode.ALL || view_mode == ViewMode.TODAY || view_mode == ViewMode.THIS_WEEK || view_mode == ViewMode.THIS_MONTH)
+	call_deferred("scroll_to_bottom")
+
+
+func scroll_to_bottom() -> void:
+	scroll_container.get_v_scroll_bar().ratio = 1.0
 
 
 func new_entry_from_task(task: Task) -> void:
@@ -74,6 +103,8 @@ func new_entry_from_task(task: Task) -> void:
 	entries[eid].set_break_duration(task.break_time_elapsed / 1000)
 	entries[eid].set_start_datetime(task.start_datetime)
 	entries[eid].set_end_datetime(task.end_datetime)
+	entries[eid].visible = bool(view_mode == ViewMode.ALL || view_mode == ViewMode.TODAY || view_mode == ViewMode.THIS_WEEK || view_mode == ViewMode.THIS_MONTH)
+	call_deferred("scroll_to_bottom")
 
 
 func set_entry_data(eid: int, entry_dict: Dictionary) -> void:
